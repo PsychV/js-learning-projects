093bec9e1b56b6dd02aab76c72546bdaf20ba11e
Victor Luque <victor.luque@GRUPATLANTIDA.GRUPATLANTIDA.CAT>
2022-06-15 13:53:51 +0200

    Practice constructor JS

---
+++ 2-js-basics/5-constructor.js
@@ -0,0 +1,14 @@
+// the main purpose of constructors is to implement 
+// reusable object creation code.
+
+//in JS they are technically regular functions, with 2 conventions:
+//1. they are named with a capital letter first
+//2. they should be executed only with the new operator
+
+function User(name) {
+    this.name = name;
+    this.isAdmin = false;
+  }
+  
+  let user = new User("Jack");
+  
