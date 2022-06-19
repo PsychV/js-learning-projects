a96a94bb9e91a9aade84499207261d26c068b521
Victor Luque <victorluquebuxade@protonmail.com>
2022-06-19 09:41:06 +0200

    Revert "eliminate debugging browser and install airbnb JS style guide with ESlint"
    This reverts commit 47d81632167298261004e6a6e88062ad76d52344.

---
+++ 1-debugging-browser/index.js
@@ -0,0 +1,5 @@
+"use strict"; //must be at the top, uses modern JS mode
+
+let message = "Hello World!";
+
+alert(message);
--- 1-js-basics/8-object-to-primitive.js
+++
@@ -1,8 +0,0 @@
-//Object to primitive conversion
-// JS doesn’t allow you to customize how operators work on objects.
-// E.g. we can’t make objects representing vectors or matrices (or achievements or whatever), add them and expect a “summed” object as the result.
-
-
-// Conversion rules
-
-
